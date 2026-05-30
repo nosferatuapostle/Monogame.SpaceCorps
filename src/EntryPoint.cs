3f546f126870a67b584645e4d0bfd2aeafa19921
2f26ef8 <nosferatudeathless@gmail.com>
2026-05-31 01:11:52 +0300

    test

--- src/EntryPoint.cs
+++ src/EntryPoint.cs
@@ -19,28 +19,36 @@ public class EntryPoint : Core
         base.Initialize();
     }
 
+    public const string UNIT_BIOMANTES_SCOUT_BASE = "unit_biomantes_scout_base";
+
+    public static Dictionary<string, SpriteSheet> ssheets = [];
+
     public static AnimatedSprite CreateAnimatedSprite(string assetName, int w, int h, int frameCount, double frameTime, bool loop = true, string name = "base")
     {
-        var tex = Content.Load<Texture2D>(assetName);
-        var atlas = Texture2DAtlas.Create("atlas\\" + assetName, tex, w, h);
-        var ss = new SpriteSheet("spritesheet\\" + assetName, atlas);
-
-        ss.DefineAnimation(name, builder =>
+        if (!ssheets.ContainsKey(assetName))
         {
-            builder.IsLooping(loop);
-            for (int i = 0; i < frameCount; i++)
-            {
-                builder.AddFrame(i, TimeSpan.FromSeconds(frameTime));
-            }
-        });
+            var tex = Content.Load<Texture2D>(assetName);
+            var atlas = Texture2DAtlas.Create("atlas\\" + assetName, tex, w, h);
+            var ss = new SpriteSheet("spritesheet\\" + assetName, atlas);
 
-        return new AnimatedSprite(ss, name);
+            ss.DefineAnimation(name, builder =>
+            {
+                builder.IsLooping(loop);
+                for (int i = 0; i < frameCount; i++)
+                {
+                    builder.AddFrame(i, TimeSpan.FromSeconds(frameTime));
+                }
+            });
+
+            ssheets[assetName] = ss;
+        }
+
+        return new AnimatedSprite(ssheets[assetName], name);
     }
 
     protected override void LoadContent()
     {
-        var assetName = "unit_biomantes_scout_base";
-        var sprite = CreateAnimatedSprite(assetName, 64, 64, 7, 0.1);
+        var sprite = CreateAnimatedSprite(UNIT_BIOMANTES_SCOUT_BASE, 64, 64, 7, 0.1);
 
         var uv = new UnitValues
         {
@@ -56,12 +64,42 @@ public class EntryPoint : Core
         base.LoadContent();
     }
 
+    List<Unit> anotherUnits = [];
+
+    int index = 0;
+    int nextPosition = 1;
+
     protected override void Update(GameTime gt)
     {
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
         unit.Update(gt);
+
+        index++;
+
+        if (index % 200 == 0)
+        {
+            var s = CreateAnimatedSprite(UNIT_BIOMANTES_SCOUT_BASE, 64, 64, 7, 0.1);
+            var unit = new Unit(s, new UnitValues
+            {
+                Health = 100f,
+                MaxHealth = 100f,
+                Speed = 100f
+            });
+
+            unit.Transform.Position = new Vector2(nextPosition * 100, 200);
+            nextPosition++;
+
+            anotherUnits.Add(unit);
+
+            index = 0;
+        }
+
+        foreach (var u in anotherUnits)
+        {
+            u.Update(gt);
+        }
 
         base.Update(gt);
     }
@@ -73,6 +111,11 @@ public class EntryPoint : Core
         SpriteBatch.Begin();
 
         unit.Draw(SpriteBatch);
+
+        foreach (var u in anotherUnits)
+        {
+            u.Draw(SpriteBatch);
+        }
 
         SpriteBatch.End();
 
